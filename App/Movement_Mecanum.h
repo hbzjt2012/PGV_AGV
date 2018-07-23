8f5cb6fd98ec87628560604833cffb15f08823f5
章仁辉 <513205481@qq.com>
2018-07-23 11:26:26 +0800

    no message

--- App/Movement_Mecanum.h
+++ App/Movement_Mecanum.h
@@ -4,9 +4,9 @@
 class Movement_Mecanum_Class :public Movement_Class
 {
 	Coordinate_Class Cal_Projection_Coor(const Coordinate_Class& Current_Coor_InOrigin)  override;	//计算投影坐标
-	float Cal_Destination_Displacement(const Coordinate_Class& Destination_Coor_InOrigin)  override;	//根据终点坐标在起点坐标中的坐标计算插补距离
+	float Cal_Destination_Displacement(const Coordinate_Class& Destination_Coor_InOrigin)  override;	//根据终点坐标在起点坐标中的坐标计算插补距离（一范数）
 	Velocity_Class& Assign_Velocity(const Coordinate_Class& Current_Coor_InOrigin, const float velocity)  override;	//根据期望坐标在起点坐标中的坐标，将总速度分配给各个轴
-	float Cal_Current_Coor_InOrigin(const Coordinate_Class& Current_Coor_InOrigin)  override;	//根据当前坐标计算在起点坐标系上的位移
+	float Cal_Current_Coor_InOrigin(const Coordinate_Class& Current_Coor_InOrigin)  override;	//根据当前坐标计算在起点坐标系上的位移（一范数）
 
 
 	float x_temp_InOrigin;	//终点坐标在起点坐标系上的x轴偏移
